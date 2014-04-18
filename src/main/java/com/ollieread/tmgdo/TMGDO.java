6e69b567982e12331300496690b01e4320bb44de
Ollie Read <labs@ollieread.com>
2014-04-18 22:03:09 +0100

    Modifications to work with metadata blocks and to allow for exclusion...blame TiC for this :P

--- src/main/java/com/ollieread/tmgdo/TMGDO.java
+++ src/main/java/com/ollieread/tmgdo/TMGDO.java
@@ -1,8 +1,10 @@
 package com.ollieread.tmgdo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockOre;
@@ -18,6 +20,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.GameRegistry.UniqueIdentifier;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(modid = TMGDO.MODID, name = TMGDO.MODID, version = TMGDO.VERSION)
@@ -26,6 +29,7 @@ public class TMGDO {
 	public static final String MODID = "TooManyGodDamnOres";
 	public static final String VERSION = "1.0-alpha";
 	public static ConfigCategory replacements;
+	public static ConfigCategory exclusions;
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
@@ -34,7 +38,8 @@ public class TMGDO {
 		
 		config.load();
 		
-		replacements = config.getCategory(Configuration.CATEGORY_GENERAL);
+		replacements = config.getCategory("replacements");
+		exclusions = config.getCategory("exclusions");
 		
 		config.save();
 	}
@@ -49,22 +54,31 @@ public class TMGDO {
 	public void harvestBlock(HarvestDropsEvent event)
 	{
 		if(replacements != null) {
-			ItemStack oreStack = new ItemStack(event.block);
+			ItemStack oreStack = new ItemStack(event.block, 1, event.blockMetadata);
 			int oreID = OreDictionary.getOreID(oreStack);
 			String oreName = OreDictionary.getOreName(oreID);
 			
 			if(replacements.containsKey(oreName)) {
 				String replacementName = replacements.get(oreName).getString();
+				List<String> exclusionNames = Arrays.asList(exclusions.get(oreName).getStringList());
+				UniqueIdentifier ore = GameRegistry.findUniqueIdentifierFor(oreStack.getItem());
+				
+				if(exclusionNames.contains(ore.modId + ":" + ore.name)) {
+					return;
+				}
+				
 				ItemStack replacementStack = null;
 				
 				for(ItemStack i : OreDictionary.getOres(oreID)) {
-					if(GameRegistry.findUniqueIdentifierFor(i.getItem()).modId.contains(replacementName)) {
+					UniqueIdentifier item = GameRegistry.findUniqueIdentifierFor(i.getItem());
+					if(!exclusionNames.contains(item.modId + ":" + item.name) && item.modId.contains(replacementName)) {
 						replacementStack = i.copy();
+						break;
 					}
 				}
 				
 				if(replacementStack != null) {	
-					for(int i = 0; i < event.drops.size(); i++) {
+					for(int i = 0; i < event.drops.size(); i++) {						
 						if(event.drops.get(i).isItemEqual(oreStack)) {
 							event.drops.set(i, replacementStack);
 						}
