09db1c8372d575fa85ca7155ad1906b0400db3ed
Peter Cashel <pacas00@gmail.com>
2016-09-03 17:34:22 +1000

    Port TooManyGodDamnOres to MC 1.10.2 and Forge 12.18.1.2011. Also works with Forge 12.18.1.2074

--- src/main/java/com/ollieread/tmgdo/TMGDO.java
+++ src/main/java/com/ollieread/tmgdo/TMGDO.java
@@ -2,122 +2,138 @@ package com.ollieread.tmgdo;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockOre;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.ConfigCategory;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
 import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
 import net.minecraftforge.oredict.OreDictionary;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.GameRegistry.UniqueIdentifier;
-import cpw.mods.fml.common.registry.LanguageRegistry;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = "TooManyGodDamnOres", name = "TooManyGodDamnOres", version = "${version}")
 public class TMGDO {
 
-    public static final Logger logger = LogManager.getLogger("TooManyGodDamnOres");
+	public static final Logger logger = LogManager.getLogger("TooManyGodDamnOres");
 
 	public static ConfigCategory replacements;
 	public static ConfigCategory exclusions;
-	
+
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
 		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
-		
+
 		config.load();
-		
+
 		replacements = config.getCategory("replacements");
 		exclusions = config.getCategory("exclusions");
-		
+
 		config.save();
 	}
-	
+
 	@EventHandler
 	public void init(FMLInitializationEvent event)
 	{
 		MinecraftForge.EVENT_BUS.register(this);
 	}
-	
+
 	@SubscribeEvent
 	public void harvestBlock(HarvestDropsEvent event)
 	{
-		if(replacements != null) {
-			if(event.block != null) {
-				ItemStack oreStack = new ItemStack(event.block, 1, event.blockMetadata);
-				if(oreStack.getItem() != null) {
-					int[] oreIDs = OreDictionary.getOreIDs(oreStack);
-					int oreID;
-					
-					if(oreIDs.length > 0) {
-						for(int i = 0; i < oreIDs.length; i++) {
-							oreID = oreIDs[i];
-							
-							if(oreID > -1) {
-								String oreName = OreDictionary.getOreName(oreID);
-								
-								if(oreName != null && replacements.containsKey(oreName)) {
-									String replacementName = replacements.get(oreName).getString();
-									Property exclusionDirty = exclusions.get(oreName);
-									List<String> exclusionNames = null;
-									
-									if(exclusionDirty != null) {
-										exclusionNames = Arrays.asList(exclusionDirty.getStringList());
-									} else {
-										exclusionNames = new ArrayList<String>();
-									}
-									
-									UniqueIdentifier ore = GameRegistry.findUniqueIdentifierFor(oreStack.getItem());
-									
-									if(ore == null) {
-										return;
-									}
-									
-									if(exclusionNames.contains(ore.modId + ":" + ore.name)) {
-										return;
-									}
-									
-									ItemStack replacementStack = null;
-									
-									for(ItemStack i1 : OreDictionary.getOres(oreID)) {
-										UniqueIdentifier item = GameRegistry.findUniqueIdentifierFor(i1.getItem());
-										if(item != null && !exclusionNames.contains(item.modId + ":" + item.name) && item.modId.contains(replacementName)) {
-											replacementStack = i1.copy();
-											break;
-										}
-									}
-									
-									if(replacementStack != null) {	
-										for(int i1 = 0; i1 < event.drops.size(); i1++) {						
-											if(event.drops.get(i1) != null && event.drops.get(i1).isItemEqual(oreStack)) {
-												event.drops.set(i1, replacementStack);
+		if(replacements != null && replacements.size() > 0) {	
+			if (event.getDrops() != null && event.getDrops().size() > 0) {
+				for (ItemStack s : event.getDrops() ) {
+					if (s.getItem() instanceof ItemBlock) {
+						
+						ItemStack oreStack = s;
+						
+						if(oreStack.getItem() != null) {
+							int[] oreIDs = OreDictionary.getOreIDs(oreStack);
+							int oreID;
+
+							if(oreIDs.length > 0) {
+								for(int i = 0; i < oreIDs.length; i++) {
+									oreID = oreIDs[i];
+
+									if(oreID > -1) {
+										String oreName = OreDictionary.getOreName(oreID);
+
+										if(oreName != null && replacements.containsKey(oreName)) {
+											String replacementName = replacements.get(oreName).getString();
+											Property exclusionDirty = exclusions.get(oreName);
+											List<String> exclusionNames = null;
+
+											if(exclusionDirty != null) {
+												exclusionNames = Arrays.asList(exclusionDirty.getStringList());
+											} else {
+												exclusionNames = new ArrayList<String>();
+											}
+
+											ResourceLocation ore = getNameForObject(oreStack.getItem());
+
+											if(ore == null) {
+												return;
+											}
+											
+											if (ore.getResourceDomain().toLowerCase().equals(replacementName.toLowerCase())) {
+												return;
+											}
+
+											if(exclusionNames.contains(ore.getResourceDomain() + ":" + ore.getResourcePath())) {
+												return;
+											}
+
+											ItemStack replacementStack = null;
+
+											for(ItemStack i1 : OreDictionary.getOres(oreName)) {
+												ResourceLocation item = getNameForObject(i1.getItem());
+												if(item != null && !exclusionNames.contains(item.getResourceDomain() + ":" + item.getResourcePath()) && item.getResourceDomain().toLowerCase().equals(replacementName.toLowerCase())) {
+													replacementStack = i1.copy();
+													break;
+												}
+											}
+
+											if(replacementStack != null) {	
+												event.getDrops().add(replacementStack);
+												event.getDrops().remove(s);
 											}
 										}
 									}
 								}
 							}
+						} else {
+							logger.warn("A HarvestDropsEvent was fired with no subject block");
 						}
 					}
-				} else {
-					logger.warn("A HarvestDropsEvent was fired with no subject block");
 				}
 			}
 		}
 	}
-	
+
+
+	public ResourceLocation getNameForObject(Item item) {		
+
+		if (item instanceof ItemBlock) {
+			return Block.REGISTRY.getNameForObject(((ItemBlock) item).getBlock());
+		} else if (item instanceof Item) {
+			return Item.REGISTRY.getNameForObject(item);
+		}		
+
+		return null;		
+	}
+
 }
